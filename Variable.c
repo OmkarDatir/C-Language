6895488224a6017a936fb92e9755b2a8e457de1c
Omkar Datir <102588631+OmkarDatir@users.noreply.github.com>
2024-06-02 17:51:33 +0530

    Add files via upload

---
+++ Variable.c
@@ -0,0 +1,67 @@
+/*#include <stdio.h>
+int main()
+{
+	int a,b;
+	 a = 5;
+         b = 8;
+	printf("Value of a is %d \n",a);
+	printf("Value of b is %d \n",b);
+	return 0;
+}
+
+//USER INPUT
+
+#include <stdio.h>
+int main()
+{
+	int a=3,b=6;
+	printf("Enter number of a: , b: ");
+	scanf("%d %d",&a,&b);
+	printf("Enter number of b: ");
+	scanf("%d",&b);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int a=10,b=2;
+	printf("a: %d, b: %d \n",b,a);
+	
+	//scanf("%d %d",&a,&b);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	a = 4;
+	printf("%d",a); //kiti number of value ghetl te return krto
+	return 0;
+
+}*/
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter a number: ");
+	scanf("%d %d",&a,&b);  //user kadun kiti input ghetl te return krto
+	
+	a=a+b;
+	printf("Sum is: %d \n",a);
+	return 0;
+	
+
+}
+
+
+
+
+
+
+
+ 
