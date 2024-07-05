93ce70f80bfe4bf85b6a22f935e395f09339c9e7
Omkar Datir <102588631+OmkarDatir@users.noreply.github.com>
2024-07-05 15:20:56 +0530

    Add files via upload

---
+++ Operator.c
@@ -0,0 +1,126 @@
+
+/*#include <stdio.h>
+int main()
+{
+	int a,b; 
+	a = 13;
+	b = 2;
+        printf("a/b: %d \n", a/b);
+        return 0;
+}
+
+//USER INPUT
+
+#include <stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter number a: ");
+	scanf("%d",&a);
+	printf("Enter number b: ");
+	scanf("%d",&b);
+	sum = a+b;
+	printf("a+b: %d \n", sum);
+	printf("a-b: %d \n", a-b);
+	printf("a*b: %d \n", a*b);
+        printf("a/b: %d \n", a/b);
+        printf("a%b: %d \n", a%b);
+	return 0;
+}
+	
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	a=2;
+	b=4;
+	a=a+b;
+	printf("a = %d \n",a);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	a=2;
+	b=4;
+	
+	a=a%b;
+	printf("a = %d \n",a);
+
+	return 0;	
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
