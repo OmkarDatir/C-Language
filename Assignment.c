dad91eeb382ba8121d6f692c33ef134596d1009d
Omkar Datir <102588631+OmkarDatir@users.noreply.github.com>
2024-08-02 22:18:55 +0530

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,47 @@
+//Assign operator (=)
+
+/*#include <stdio.h>
+int main()
+{
+	int a,b;
+	a = 4;
+	printf("%d",a);
+	return 0;
+}
+
+
+//ADD AND Assignment (+=)
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	a = 5;
+	printf("%d",a+=1);
+	return 0;
+}
+
+//Substract AND Assignment (-=)
+
+#include<stdio.h>
+int main()
+{
+	int Z;
+	Z = 22;
+	printf("%d",Z-=2);
+	return 0;
+}*/
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
