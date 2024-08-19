a3b0ca6947c8f7f05b17a4716bbe7a72eb03fed0
Omkar Datir <102588631+OmkarDatir@users.noreply.github.com>
2024-08-19 08:17:33 +0530

    Add files via upload

---
+++ Constant.c
@@ -0,0 +1,86 @@
+//Variables..................................
+
+/*#include <stdio.h>
+int main()
+{
+	
+	int a=2002;
+	printf("%d",a);
+		
+	return 0;
+}
+
+
+// Constant.................................
+
+#include <stdio.h>
+int main()
+{
+	const int pi=3.14;
+	printf("%d",pi);
+
+	return 0;
+}
+
+//...........................................
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	a=2;
+	b=3;
+	
+	a=a+b;
+	printf("%d",a);
+	return 0;
+}*/
+
+//.............................................
+
+#include <stdio.h>
+int main()
+{
+	const int a=3;
+	int b=2;
+
+	b=a+b;
+	printf("%d",b);
+
+	a=b+a;
+	printf("%d",a);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
