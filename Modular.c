b21a14e4bf13b5c3d3b91ea03af2187af7c71188
Omkar Datir <102588631+OmkarDatir@users.noreply.github.com>
2024-11-27 21:15:07 +0530

    Add files via upload

---
+++ Modular.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main()
+{
+	int a,b;
+	a = 25;
+	b = 5;
+	printf("a%b: %d \n", a%b);
+	return 0;
+}*/
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a = 10;
+	b = 25;
+	printf("a%b: %d \n", a%b);
+	return 0;
+}
